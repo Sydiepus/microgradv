6977f2254043e8f2862be9a25e6c77c4450ec4e6
Charbel Assaad <sydiepus@gmail.com>
2024-04-02 22:08:43 +0300

    feat: mul, pow op and backpropagation

--- src/micrograd.v
+++ src/micrograd.v
@@ -3,72 +3,80 @@ module microgradv
 import math
 
 pub struct Value {
+mut:
+	val_backward fn () = fn () {
+		return
+	}
 pub:
-        data f64 @[required]
+	data f64    @[required]
+	op   string // default empty
 pub mut:
-        parents []&Value // default empty
-        op      string   // default empty
-        grad    f64 = 0.0 // default 0.0
+	parents []&Value // default empty
+	grad    f64 = 0.0 // default 0.0
 }
 
-pub fn (a &Value) add(b &Value) Value {
-        mut out := Value{
-                data: a.data + b.data
-                parents: [a, b]
-                op: '+'
-        }
-        return out
+pub fn (a &Value) add(b &Value) &Value {
+	mut out := &Value{
+		data: a.data + b.data
+		parents: [a, b]
+		op: '+'
+	}
+	out.val_backward = fn [mut out] () {
+		out.parents[0].grad += out.grad
+		out.parents[1].grad += out.grad
+	}
+	return out
 }
 
-pub fn (a &Value) mul(b &Value) Value {
-        mut out := Value{
-                data: a.data * b.data
-                parents: [a, b]
-                op: '*'
-        }
-        return out
+pub fn (a &Value) mul(b &Value) &Value {
+	mut out := &Value{
+		data: a.data * b.data
+		parents: [a, b]
+		op: '*'
+	}
+	out.val_backward = fn [mut out] () {
+		out.parents[0].grad += out.parents[1].data * out.grad
+		out.parents[1].grad += out.parents[0].data * out.grad
+	}
+
+	return out
 }
 
-pub fn (a &Value) pow(b int) Value {
-        mut out := Value{
-                data: math.pow(a.data, b)
-                parents: [&Value(a)]
-                op: '**'
-        }
-        return out
-}
-
-fn (mut a Value) p_backward() {
-        // Make sure the struct have 2 parents
-        if a.op == '+' {
-                a.parents[0].grad += a.grad
-                a.parents[1].grad += a.grad
-        }
+pub fn (a &Value) pow(b f64) &Value {
+	mut out := &Value{
+		data: math.pow(a.data, b)
+		parents: [a]
+		op: '**'
+	}
+	out.val_backward = fn [mut out, b] () {
+		out.parents[0].grad += (b * math.pow(out.parents[0].data, b - f64(1))) * out.grad
+	}
+	return out
 }
 
 pub fn (mut a Value) backward() {
-		
-		// build topologocal order
-		// parents will become childrens.
-		mut childrens := []&Value{}
-		mut visited := []&Value{}
-		build_topo(a, mut childrens, mut visited)
-		a.grad = 1
-		for mut child in childrens {
-			child.p_backward()
-		}
+	// build topological order
+	// parents will become children.
+	mut children := []&Value{}
+	mut visited := []&Value{}
+	build_topo(a, mut children, mut visited)
+	a.grad = 1
+	for child in children {
+		child.val_backward()
+	}
 }
 
-fn build_topo(a &Value, mut childrens []&Value, mut visited []&Value) {
-		if a !in visited {
-			visited.insert(0, a)
-			for parent in a.parents {
-				build_topo(parent, mut childrens, mut visited)
-			}
-			// by inserting at the first position, at the end we will have the parents of the value which backward was called on at the beginingg
-			childrens.insert(0, a)
-		} 
+fn build_topo(a &Value, mut children []&Value, mut visited []&Value) {
+	if a !in visited {
+		visited.insert(0, a)
+		for parent in a.parents {
+			build_topo(parent, mut children, mut visited)
+		}
+		// by inserting at the first position, at the end we will have the parents of the value which backward was called on at the beginning
+		children.insert(0, a)
+	}
 }
+
 pub fn value(a f64) &Value {
 	return &Value{
 		data: a
