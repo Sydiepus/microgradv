cb7795ecd49d641e9e254426e36f059981b75781
Charbel Assaad <sydiepus@gmail.com>
2024-04-17 11:34:00 +0300

    feat: add ReLu, division,and subtraction

--- src/micrograd.v
+++ src/micrograd.v
@@ -54,6 +54,53 @@ pub fn (a &Value) pow(b f64) &Value {
 	return out
 }
 
+pub fn (a &Value) sub(b &Value) &Value {
+	mut out := &Value{
+		data: a.data - b.data
+		parents: [a, b]
+		op: '-'
+	}
+	out.val_backward = fn [mut out] () {
+		out.parents[0].grad += out.grad
+		out.parents[1].grad -= out.grad
+	}
+	return out
+}
+
+pub fn (a &Value) div(b &Value) &Value {
+	mut out := &Value{
+		data: a.data / b.data
+		parents: [a, b]
+		op: '/'
+	}
+	out.val_backward = fn [mut out] () {
+		out.parents[0].grad += math.pow(out.parents[1].data, -1) * out.grad
+		out.parents[1].grad += f64(-1) * out.parents[0].data * math.pow(out.parents[1].data,
+			-2) * out.grad
+	}
+	return out
+}
+
+pub fn (a &Value) relu() &Value {
+	mut rel := a.data
+	if a.data < 0 {
+		rel = 0
+	}
+	mut out := &Value{
+		data: rel
+		parents: [a]
+		op: 'ReLu'
+	}
+	out.val_backward = fn [mut out] () {
+		if out.data > 0 {
+			out.parents[0].grad += out.grad * 1
+		} else {
+			out.parents[0].grad += 0
+		}
+	}
+	return out
+}
+
 pub fn (mut a Value) backward() {
 	// build topological order
 	// parents will become children.
